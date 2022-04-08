4d107be190de3f68b29a71f831b3eca28bec4659
zoemanning <irlandazoe.mm@gmail.com>
2022-04-08 10:32:51 -0400

    updated

--- src/main/java/manning/irlanda/domain/weather/services/WeatherServiceImpl.java
+++ src/main/java/manning/irlanda/domain/weather/services/WeatherServiceImpl.java
@@ -2,6 +2,7 @@ package manning.irlanda.domain.weather.services;
 
 import lombok.extern.slf4j.Slf4j;
 import manning.irlanda.domain.weather.models.WeatherApiResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -16,6 +17,9 @@ public class WeatherServiceImpl implements WeatherService {
 
     private RestTemplate restTemplate;
 
+    @Value("${weather.api.key}")
+    private String apiKey;
+
     public WeatherServiceImpl() {
         restTemplate = new RestTemplate();
     }
@@ -23,7 +27,6 @@ public class WeatherServiceImpl implements WeatherService {
     @Override
     public Optional<WeatherApiResponse> requestDataFromApi(String lat, String lon) {
         try {
-            String apiKey = "9ea8c63f6dbfa7e330cc96c383899eb1";
             String url = "https://api.openweathermap.org/data/2.5/weather?lat=%s&lon=%s&appid=%s";
             String requestUrl = String.format(url, lat, lon, apiKey);
             ResponseEntity<WeatherApiResponse> response = restTemplate.exchange(requestUrl, HttpMethod.GET, null, WeatherApiResponse.class);
